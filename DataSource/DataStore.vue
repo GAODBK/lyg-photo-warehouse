90b899da453ea33ddd923ed935948208eaca2588
GAODBK <154538773+GAODBK@users.noreply.github.com>
2024-10-05 11:09:02 +0800

    上传了数据集
    将 川菜项目 手机项目 果蔬项目 的data数据集合，上传上来了

---
+++ DataSource/DataStore.vue
@@ -0,0 +1,207 @@
+<script setup>
+import {ref} from "vue";
+import ShopByCategory from './ShopByCategory.vue'
+
+const foodData = ref([
+  {
+    f_name: "绿芦笋",
+    price: 23,
+
+    imgUrl: 'src/images/1green-asparag.jpg',
+    desc: ["蔬菜"]
+  },
+  {
+    f_name: "卷心菜",
+    price: 23,
+
+    imgUrl: 'src/images/2kohlrabi.jpg',
+    desc: ["蔬菜", "苤", "德国萝卜", "野白菜"]
+  },
+  {
+    f_name: "大蒜",
+    price: 23,
+
+    imgUrl: 'src/images/3garlic.jpg',
+    desc: ["蔬菜", "香料"]
+  },
+  {
+    f_name: "生菜叶",
+    price: 23,
+
+    imgUrl: 'src/images/4green.jpg',
+    desc: ["蔬菜", "收成", "叶子"]
+  },
+  {
+    f_name: "番茄 ",
+    price: 23,
+
+    imgUrl: 'src/images/5tomatoe.jpg',
+    desc: ["蔬菜", "罗勒", "意大利美食", "有机的"]
+  },
+  {
+    f_name: "西葫芦",
+    price: 23,
+
+    imgUrl: 'src/images/7zucchinis.jpg',
+    desc: ["蔬菜", "新鲜的", "黄色"]
+  },
+  {
+    f_name: "南瓜",
+    price: 23,
+
+    imgUrl: 'src/images/8pumpkins.jpg',
+    desc: ["蔬菜", "装饰葫芦科", "收成", "生产"]
+  },
+
+  {
+    f_name: "土豆",
+    price: 23,
+
+    imgUrl: 'src/images/6potatoe.jpg',
+    desc: ["蔬菜", "豆芽", "收成", "新鲜的"]
+  },
+  {
+    f_name: "布鲁塞尔豆芽",
+    price: 23,
+
+    imgUrl: 'src/images/9brussels-sprouts.jpg',
+    desc: ["蔬菜", "豆芽", "收成", "新鲜的"]
+  },
+
+
+  {
+    f_name: "黑莓",
+    price: 23,
+
+    imgUrl: 'src/images/11berry.jpg',
+    desc: ["水果", "浆果", "维生素"]
+  },
+  {
+    f_name: "蓝莓",
+    price: 23,
+
+    imgUrl: 'src/images/12blueberries.jpg',
+    desc: ["水果", "营养", "健康", "新鲜的"]
+  },
+  {
+    f_name: "草莓",
+    price: 23,
+
+    imgUrl: 'src/images/13strawberries.jpg',
+    desc: ["水果", "健康", "维生素"]
+  },
+  {
+    f_name: "山莓",
+    price: 23,
+
+    imgUrl: 'src/images/14fruits.jpg',
+    desc: ["水果", "浆果", "成熟", "新鲜的"]
+  },
+  {
+    f_name: "橙子",
+    price: 23,
+
+    imgUrl: 'src/images/16oranges.jpg',
+    desc: ["水果", "收成", "新鲜的"]
+  },
+  {
+    f_name: "桃子",
+    price: 23,
+
+    imgUrl: 'src/images/15peache.jpg',
+    desc: ["水果", "新鲜的", "维生素"]
+  },
+  {
+    f_name: "山莓",
+    price: 23,
+
+    imgUrl: 'src/images/17raspberries.jpg',
+    desc: ["水果", "新鲜的", "健康", "营养"]
+  },
+
+
+  {
+    f_name: "pork food",
+    price: 23,
+
+    imgUrl: 'src/images/21steamed-pork.jpg',
+    desc: ["肉制品", "pork"]
+  },
+  {
+    f_name: "香肠",
+    price: 23,
+
+    imgUrl: 'src/images/23sausages.jpg',
+    desc: ["肉制品", "烧烤", "炙烤"]
+  },
+  {
+    f_name: "牛排",
+    price: 23,
+
+    imgUrl: 'src/images/24meat.jpg',
+    desc: ["肉制品", "烤羊肉", "蛋白质"]
+  },
+  {
+    f_name: "火腿",
+    price: 23,
+
+    imgUrl: 'src/images/22ham.jpg',
+    desc: ["肉制品", "肉", "帕塔内格拉"]
+  },
+  {
+    f_name: "鸭脯",
+    price: 23,
+
+    imgUrl: 'src/images/25meat.jpg',
+    desc: ["肉制品", "生的", "可口"]
+  },
+  {
+    f_name: "鸡肉",
+    price: 23,
+
+    imgUrl: 'src/images/26chicken.jpg',
+    desc: ["肉制品", "烤箱食品", "一顿饭"]
+  },
+
+
+  {
+    f_name: "苹果汁",
+    price: 23,
+
+    imgUrl: 'src/images/31punch.jpg',
+    desc: ["饮料", "烤箱食品", "一顿饭"]
+  },
+  {
+    f_name: "热葡萄酒",
+    price: 23,
+
+    imgUrl: 'src/images/32mulled-wine.jpg',
+    desc: ["饮料"]
+  },
+  {
+    f_name: "桔子汁",
+    price: 23,
+
+    imgUrl: 'src/images/33orange-juice.jpg',
+    desc: ["饮料"]
+  },
+  {
+    f_name: "葡萄柚汁",
+    price: 23,
+
+    imgUrl: 'src/images/34cocktails.jpg',
+    desc: ["饮料"]
+  },
+  {
+    f_name: "醋栗汁汽水",
+    price: 23,
+
+    imgUrl: 'src/images/35drink.jpg',
+    desc: ["饮料"]
+  }
+])
+</script>
+
+<template>
+  <ShopByCategory :foodData="foodData"/>
+</template>
---
+++ DataSource/dataSource.vue
@@ -0,0 +1,161 @@
+<script setup>
+import { ref } from "vue";
+import categories from "./categories.vue";
+const mobileDatabase = ref([
+  {
+    m_name: "Galaxy Note10+ 5G",
+    imgUrl: "src/images/6d2x.png",
+    desc: "/Samsung",
+    price: 2800,
+    quantity: 1,
+  },
+  {
+    m_name: "Galaxy S10",
+    imgUrl: "src/images/7beyond1lte.png",
+    desc: "/Samsung",
+    price: 2800,
+    quantity: 1,
+  },
+  {
+    m_name: "A73 5G",
+    imgUrl: "src/images/8a73xq.png",
+    desc: "/Samsung",
+    price: 2800,
+    quantity: 1,
+  },
+  {
+    m_name: "A52 4g",
+    imgUrl: "src/images/9a52q.png",
+    desc: "/Samsung",
+    price: 2800,
+    quantity: 1,
+  },
+
+
+
+  {
+    m_name: "Realme 5s",
+    imgUrl: "src/images/11r5x.png",
+    desc: "/realme",
+    price: 2800,
+    quantity: 1,
+  },
+  {
+    m_name: "Realme 2 Pro",
+    imgUrl: "src/images/12r2p.png",
+    desc: "/realme",
+    price: 2800,
+    quantity: 1,
+  },
+  {
+    m_name: "Realme 6 Pro",
+    imgUrl: "src/images/13RMX2061.png",
+    desc: "/realme",
+    price: 2800,
+    quantity: 1,
+  },
+  {
+    m_name: "Realme 5 Pro",
+    desc: "/realme",
+    imgUrl: "src/images/14RMX1971.png",
+    price: 2800,
+    quantity: 1,
+  },
+
+
+
+  {
+    m_name: "OnePlus Nord",
+    imgUrl: "src/images/15avicii.png",
+    desc: "/OnePlus",
+    price: 2800,
+    quantity: 1,
+  },
+  {
+    m_name: "OnePlus 5",
+    imgUrl: "src/images/16dre.png",
+    desc: "/OnePlus",
+    price: 2800,
+    quantity: 1,
+  },
+  {
+    m_name: "OnePlus Nord N200",
+    imgUrl: "src/images/17dumpling.png",
+    desc: "/OnePlus",
+    price: 2800,
+    quantity: 1,
+  },
+  {
+    m_name: "OnePlus 5T",
+    imgUrl: "src/images/18fajita.png",
+    desc: "/OnePlus",
+    price: 2800,
+    quantity: 1,
+  },
+
+
+  {
+    m_name: "Pixel  5a",
+    imgUrl: "src/images/21barbet.png",
+    desc: "/Pixel",
+    price: 2800,
+    quantity: 1,
+  },
+  {
+    m_name: "Pixel  3",
+    imgUrl: "src/images/22blueline.png",
+    desc: "/Pixel",
+    price: 2800,
+    quantity: 1,
+  },
+  {
+    m_name: "Pixel  3A XL",
+    imgUrl: "src/images/23bonito.png",
+    desc: "/Pixel",
+    price: 2800,
+    quantity: 1,
+  },
+  {
+    m_name: "Pixel  XL",
+    imgUrl: "src/images/24marlin.png",
+    desc: "/Pixel",
+    price: 2800,
+    quantity: 1,
+  },
+
+
+
+  {
+    m_name: "Moto Z2 Play",
+    imgUrl: "src/images/25albus.png",
+    desc: "/Motorola",
+    price: 2800,
+    quantity: 1,
+  },
+  {
+    m_name: "Moto G6",
+    imgUrl: "src/images/26ali.png",
+    desc: "/Motorola",
+    price: 2800,
+    quantity: 1,
+  },
+  {
+    m_name: "Moto Z3 Play",
+    imgUrl: "src/images/27beckham.png",
+    desc: "/Motorola",
+    price: 2800,
+    quantity: 1,
+  },
+  {
+    m_name: "Moto One Power",
+    imgUrl: "src/images/28chef.png",
+    desc: "/Motorola",
+    price: 2800,
+    quantity: 1,
+  },
+]);
+</script>
+
+<template>
+  <categories :mobileDatabase="mobileDatabase" />
+</template>
